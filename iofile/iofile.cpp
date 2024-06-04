82e321c5d306b11222a12c60a7fffcb8f9b9fe38
Weellyyy <wildanprtm2304@gmail.com>
2024-06-04 17:26:02 +0700

    unlimited loop untuk menulis

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -13,4 +13,11 @@ int main() {
 	ofstream outfile;
 	//menunjuk ke sebuah nama file
 	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+	}
 }
