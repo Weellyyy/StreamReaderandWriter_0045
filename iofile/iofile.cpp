8e1190fdec093b21e58221ca2a0759ee03517a6e
Weellyyy <wildanprtm2304@gmail.com>
2024-06-04 17:20:11 +0700

    menambahkan using namespace std

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -1,11 +1,14 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
+using namespace std;
 
 
 
 
 int main() {
+	string baris;
 
+	//membuka file dalam mode menulis.
+	ofstream outfile;
 }
